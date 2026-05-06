34589b0a8adcad0fb12be63940ad2412ba6cef76
kanon8888 <mdrakibhasankanon@gmail.com>
2026-05-07 00:38:42 +0600

    seven commit

--- solutions.ts
+++ solutions.ts
@@ -98,3 +98,11 @@ console.log(student.getDetails());
 
 
 
+
+// blog-7.md
+
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+    return arr1.filter((num) => arr2.includes(num));
+}
+
+console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]));
